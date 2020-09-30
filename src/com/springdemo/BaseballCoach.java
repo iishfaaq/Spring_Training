10ee73d642c1d6bd9120d4a98c5d47b7385af98a
Ishfaaq Ismath <iishfaaq@gmail.com>
2020-09-30 18:24:04 +0530

    Initial commit

---
+++ src/com/springdemo/BaseballCoach.java
@@ -0,0 +1,11 @@
+package com.springdemo;
+
+public class BaseballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout () {
+		
+		return "Spend 30 minitues on batting practice";
+	}
+
+}
---
+++ src/com/springdemo/MyApp.java
@@ -0,0 +1,21 @@
+package com.springdemo;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		Coach TheCoach = new BaseballCoach();
+		Coach TheCoach2 = new TrackCoach();
+		
+		
+		System.out.println(TheCoach.getDailyWorkout());
+		System.out.println(TheCoach2.getDailyWorkout());
+		System.out.println(TheCoach2.getDailyWorkout());
+		System.out.println(f);
+		
+	}
+
+}
