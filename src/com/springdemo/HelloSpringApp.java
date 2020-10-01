60ee71353e020dd01420c5f4ce622c893aaf27d2
Ishfaaq Ismath <iishfaaq@gmail.com>
2020-10-01 16:01:27 +0530

    Added ApplicationContext.xml file

---
+++ src/com/springdemo/HelloSpringApp.java
@@ -0,0 +1,19 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		theCoach.getDailyWorkout();
+		
+		context.close();
+
+	}
+
+}
