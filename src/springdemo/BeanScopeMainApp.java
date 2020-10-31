418dcb6d5911dc64bf21e0eda40dee5aa7c4798c
Ishfaaq Ismath <iishfaaq@gmail.com>
2020-10-31 16:15:05 +0530

    bean scope training

--- src/springdemo/BeanScopeMainApp.java
+++ src/springdemo/BeanScopeMainApp.java
@@ -11,6 +11,17 @@ public class BeanScopeMainApp {
 		
 		TrackCoach Coach2 = context.getBean(TrackCoach.class);
 		
+		BaseballCoach Coach3 = context.getBean(BaseballCoach.class);
+		
+		BaseballCoach Coach4 = context.getBean(BaseballCoach.class);
+		
+		boolean res = (Coach == Coach2);
+		boolean res2 = (Coach3 == Coach4);
+		
+		System.out.println(res);  // this will be true because scope of the trackcoach is singleton by default
+		System.out.println(res2); // this will be false because we changed the scope of the BaseballCoach is prototype 
+								  // Which will create multiple objects
+		
 		context.close();
 
 	}
